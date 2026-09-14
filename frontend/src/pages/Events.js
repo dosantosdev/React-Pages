1fd64c7528cd369a9fc7218611784e5cf86dbcfb
dosantosdev <dosantosdev@gmail.com>
2026-09-14 17:05:38 -0300

    feat: Add newsletter functionality; implement newsletter signup component and routing

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,25 +1,23 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, Await } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 function EventsPage() {
-  const data = useLoaderData();
-
-  // if (data.isError) {
-  //   return <p>{data.message}</p>;
-  // }
-  const events = data.events;
+  const { events } = useLoaderData();
 
   return (
-    <>
-      <EventsList events={events} />
-    </>
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
   );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
@@ -31,6 +29,14 @@ export async function loader() {
       { message: "Could not fetch events." },
       { status: 500 },
     );
+  } else {
+    const resData = await response.json();
+    return resData.events;
   }
-  return response.json();
+}
+
+export function loader() {
+  return {
+    events: loadEvents(),
+  };
 }
